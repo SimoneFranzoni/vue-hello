0b67e369b3163905d109f4c1733829478dbafc3d
SimoneFranzoni <simofranzoni99@gmail.com>
2021-11-10 16:00:59 +0100

    image

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ const app = new Vue({
     el: '#app',
 
     data: {
-        messaggio: 'Hello There'
-        
+        messaggio: 'Hello There',
+        image: 'https://nerdbot.com/wp-content/uploads/2020/01/1r3fn3-e1579990017466-1200x761.jpg'
     },
     
 })
